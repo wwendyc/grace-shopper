63d374d9eb53027e8ad6bf6fecc5f89e20624f18
Blake Zeiger <blakezeiger@gmail.com>
2018-04-17 11:30:37 -0400

    product model created, imported and exported product from /server/db/index

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,11 @@
 const db = require('./database')
 const User = require('./user')
+const Product = require('./product')
 
 // associations go here!
 
 module.exports = {
   db,
-  User
+  User,
+  Product
 }
