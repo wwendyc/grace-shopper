fce1bf7765aeeed05f4a9ee76f89864d073a8d39
Robert Goon <rob.goon@gmail.com>
2018-04-17 12:27:52 -0400

    added order model and assocs

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,13 @@
 const db = require('./database')
 const User = require('./user')
+const Order = require('./order')
 
 // associations go here!
+Order.belongsTo(User)
+User.hasMany(Order)
 
 module.exports = {
   db,
-  User
+  User,
+  Order
 }
