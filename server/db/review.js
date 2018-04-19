8ef3b24f8772dd4fd5cbfb4a08db7959f71dc8f8
elza-abraham <elzaeltho@gmail.com>
2018-04-19 12:59:25 -0700

    test for review model

--- server/db/review.js
+++ server/db/review.js
@@ -5,10 +5,7 @@ const Review = db.define('review', {
   review: {
     type: Sequelize.TEXT,
     validate: {
-      min: {
-        args: [10],
-        msg: 'Minimum 10 characters required for review'
-      }
+      len: [11]
     }
   },
   rating: {
