5f63ecb15610ea9603e55d3bdaf4677eabcacd9a
elza-abraham <elzaeltho@gmail.com>
2018-04-23 14:48:56 -0700

    few more links added for navigation

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -5,6 +5,7 @@ import {AuthLink, Logout} from './auth'
 const Navbar = () => (
   <nav className='row center-y'>
     <Link to='/'><img id='logo' src='/favicon.ico' /></Link>
+    <Link to='/'>Shop</Link>
     <Link to='/login'>Login</Link>
     <Link to='/signup'>Signup</Link>
     <AuthLink to='/home'>Home</AuthLink>
