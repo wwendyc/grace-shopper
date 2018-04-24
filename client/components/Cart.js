8391e7906c8632b439e1efd108337d306c279d1d
Robert Goon <rob.goon@gmail.com>
2018-04-24 12:35:25 -0400

    Checkout v1.01

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import {getProducts} from '../store/product'
 import {getCart, removeFromCart} from '../store/cart'
 
 const Cart = props => {
