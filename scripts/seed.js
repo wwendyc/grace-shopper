f945de84b2e26a1be5820fe0541bdb92a3474118
wwendyc <wendy.changw@gmail.com>
2018-04-25 11:36:38 -0400

    updated seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -16,7 +16,7 @@ const seed = async () => {
 
   const products = await Promise.all([
     Product.create({name: 'Scott "Squishy" Scribbles', description: 'Squishy may look like a wide-eyed wanderer — small, sweet, naïve and quiet, but he\'s got scaring down to an art and can teach you how to do it too!', price: 50, inventoryQuantity: 11, imgUrl: 'http://hbimg.b0.upaiyun.com/a81510b8bfc47fb0a78fd590dded3d6ae99be19e2436d-13OZTA_fw658' }),
-    Product.create({name: 'Art', description: 'The ultimate free spirit, Art is a mysterious monster with a questionable background.', price: 40, inventoryQuantity: 10, imgUrl: 'https://www.conmishijos.com/assets/posts/0000/546-personajes-de-la-pelicula-monstruos-university-art.jpg' }),
+    Product.create({name: 'Fuzzy Art of Illiciting Screams', description: 'The ultimate free spirit, Art is a mysterious monster with a questionable background.', price: 40, inventoryQuantity: 10, imgUrl: 'https://www.conmishijos.com/assets/posts/0000/546-personajes-de-la-pelicula-monstruos-university-art.jpg' }),
     Product.create({name: 'Don Carlton', description: 'Midwestern sales monster turned Monsters University Scarer graduate can help you pursue a dream career in Scaring!', price: 60, inventoryQuantity: 12, imgUrl: 'https://vignette.wikia.nocookie.net/pixar/images/8/8d/MonstersUniversityDon1.png/revision/latest?cb=20130222130034' }),
     Product.create({name: 'Dean Hardscrabble', description: 'Learn from the legendary Dean Hardscrabble of the School of Scaring at Monsters University!', price: 300, inventoryQuantity: 2, imgUrl: 'http://4.bp.blogspot.com/-TXkjZ8TwoXM/USwUAy7_CSI/AAAAAAAALeg/WIkKl2knma4/s1600/Monsters_University_Dean-Hardscrabble.jpg' }),
     Product.create({name: 'Sulley\'s thunder roar', description: 'Learn how to dig deep down and let the scary out!', price: 100, inventoryQuantity: 17, imgUrl: 'https://www.conmishijos.com/assets/posts/0000/551-personajes-de-la-pelicula-monsters-university-sulley.jpg' }),
@@ -76,7 +76,7 @@ const seed = async () => {
         },
         {
           id: 4,
-          name: "Art",
+          name: "Fuzzy Art of Illiciting Screams",
           imgUrl: "https://www.conmishijos.com/assets/posts/0000/546-personajes-de-la-pelicula-monstruos-university-art.jpg",
           quantity: 3,
           price: 375,
